39af58c79f056532bc1afd471f78534685e40544
Arturo Oncevay <a.oncevay@ed.ac.uk>
2024-07-01 14:36:19 +0100

    new llama translate function

--- llama.py
+++ llama.py
@@ -9,6 +9,7 @@ os.environ['TRANSFORMERS_CACHE'] = "~/air/models/arturo"
 def llama_translate(pipeline, src_lang_name, tgt_lang_name, src_text):
     message = f"{src_lang_name}: {src_text}\n{tgt_lang_name}: "
     output = pipeline(message)
+    txt_output = output[0]["generated_text"].split("\n")[0]
     return output
 
 model_id = "meta-llama/Meta-Llama-3-8B"
@@ -22,8 +23,9 @@ pipeline = transformers.pipeline(
     device_map="cuda"
 )
 
-message = "English: Hello World!\nSpanish: "
+message = "English: The cross-check of the outcome of the economic analysis with that of the monetary analysis clearly confirms that annual inflation rates are likely to remain well above levels consistent with price stability for some time and , when taking into account the weakening of demand , that upside risks to price stability have diminished somewhat , but they have not disappeared .\nSpanish: "
 print(pipeline(message))
+print(pipeline(message)[0]["generated_text"])
 
 
 os.makedirs("results.sample50", exist_ok=True)
@@ -33,11 +35,11 @@ results = {}
 
 for lang, lang_name, _ in languages_names:
     print(lang, "en2xx")
-    results[f"en2{lang}"] = [pipeline(f"English: {t}\n{lang_name}: ").strip() for t in tqdm(dataset["en"])]
+    results[f"en2{lang}"] = [llama_translate(pipeline, "English", lang, t) for t in tqdm(dataset["en"])]
     with open(f"{prefix}.en2{lang}.txt", "w", encoding="utf-8") as f:
         f.write("\n".join(results[f"en2{lang}"]))
     print(lang, "xx2en")
-    results[f"{lang}2en"] = [pipeline(f"{lang_name}: {t}\nEnglish: ").strip() for t in tqdm(dataset[lang])]
+    results[f"{lang}2en"] = [llama_translate(pipeline, lang, "English", t) for t in tqdm(dataset[lang])]
     with open(f"{prefix}.{lang}2en.txt", "w", encoding="utf-8") as f:
         f.write("\n".join(results[f"{lang}2en"]))
         
