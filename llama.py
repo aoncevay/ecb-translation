18e16016a7ccd2047cf4409df135fef0b5c8dfb9
Arturo Oncevay <a.oncevay@ed.ac.uk>
2024-07-03 01:24:39 +0100

    prompt templates...

--- llama.py
+++ llama.py
@@ -1,5 +1,5 @@
 import os
-os.environ['TRANSFORMERS_CACHE'] = "~/air/models/arturo"
+os.environ['HF_HOME'] = "~/air/models/arturo/huggingface/hub"
 token = "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"
 
 import transformers
@@ -51,7 +51,7 @@ def translate_w_template(pipeline, messages_template, src_lang_name, tgt_lang_na
     return txt_output
 
 
-def run_llm_translate(model_id = "meta-llama/Meta-Llama-3-8B-Instruct", num_shot=1, num_sample=0, results_dir="results"):
+def run_llm_translate(model_id = "meta-llama/Meta-Llama-3-8B-Instruct", system_model=True, num_shot=1, num_sample=0, results_dir="results"):
 
     pipeline = transformers.pipeline(
         "text-generation", 
@@ -82,35 +82,43 @@ def run_llm_translate(model_id = "meta-llama/Meta-Llama-3-8B-Instruct", num_shot
 
     for lang, lang_name, _ in languages_names:
         print(lang, "en2xx")
-        messages = [
-            {"role": "system", "content": "You are a professional translator in the banking and finance domain."},
-        ]
+        messages = []
+        if system_model:
+            messages.append({"role": "system", "content": "You are a professional translator in the banking and finance domain."})
         for i in range(num_shot):
-            messages.append({"role": "user", "content": f"Translate the following text from English into {lang_name}.\nEnglish: {dataset_examples['en'][i]}\n{lang_name}: {dataset_examples[lang][i]}"})
+            messages.append({"role": "user", "content": f"Translate the following text from English into {lang_name}.\nEnglish: {dataset_examples['en'][i]}\n{lang_name}: "}) 
+            messages.append({"role": "assistant", "content": f"{dataset_examples[lang][i]}"})
+
         results[f"en2{lang}"] = [translate_w_template(pipeline, messages, "English", lang, t) for t in tqdm(dataset["en"])]
         with open(f"{prefix}.en2{lang}.txt", "w", encoding="utf-8") as f:
             f.write("\n".join(results[f"en2{lang}"]))
         
         print(lang, "xx2en")
-        messages = [
-            {"role": "system", "content": "You are a professional translator in the banking and finance domain."},
-        ]
+        messages = []
+        if system_model:
+            messages.append({"role": "system", "content": "You are a professional translator in the banking and finance domain."})
         for i in range(num_shot):
-            messages.append({"role": "user", "content": f"Translate the following text from {lang_name} into English.\n{lang_name}: {dataset_examples[lang][i]}\nEnglish: {dataset_examples['en'][i]}"})
+            messages.append({"role": "user", "content": f"Translate the following text from {lang_name} into English.\n{lang_name}: {dataset_examples[lang][i]}\nEnglish: "})
+            messages.append({"role": "assistant", "content": f"{dataset_examples['en'][i]}"})
         results[f"{lang}2en"] = [translate_w_template(pipeline, messages, lang, "English", t) for t in tqdm(dataset[lang])]
         with open(f"{prefix}.{lang}2en.txt", "w", encoding="utf-8") as f:
             f.write("\n".join(results[f"{lang}2en"]))
             
 
 if __name__ == "__main__":
-    list_models = ["meta-llama/Meta-Llama-3-8B-Instruct", "CohereForAI/aya-23-8B", "bigscience/bloomz-7b1"] #, "Unbabel/TowerInstruct-v0.2", "mistralai/Mixtral-8x7B-Instruct-v0.1", "mistralai/Mistral-7B-Instruct-v0.3"]
-    list_num_shots = [1, 5, 10]
-    for model_id in list_models:
+    list_models = ["meta-llama/Meta-Llama-3-8B-Instruct", "CohereForAI/aya-23-8B", "bigscience/bloomz-7b1", "mistralai/Mixtral-8x7B-Instruct-v0.1"] #, "Unbabel/TowerInstruct-v0.2", , "mistralai/Mistral-7B-Instruct-v0.3"]
+    list_system_model = [True, False, True, False]
+    list_num_shots = [1, 5]#, 10]
+    for model_id, system_model in zip(list_models, list_system_model):
         print("MODEL:", model_id)
         for num_shot in list_num_shots:
             print("  num_shot:", num_shot)
-            run_llm_translate(model_id=model_id, num_shot=num_shot, num_sample=50,results_dir="results.smpl50")
+            run_llm_translate(model_id=model_id, system_model= system_model, num_shot=num_shot, num_sample=5,results_dir="results.smpl50")
             cleanup()
             print()
 
 # ValueError: Could not load model mistralai/Mixtral-8x7B-Instruct-v0.1 with any of the following classes: (<class 'transformers.models.auto.modeling_auto.AutoModelForCausalLM'>, <class 'transformers.models.mixtral.modeling_mixtral.MixtralForCausalLM'>).
+# Aya: jinja2.exceptions.TemplateError: Conversation roles must alternate user/assistant/user/assistant/...
+# You seem to be using the pipelines sequentially on GPU. In order to maximize efficiency please use a dataset
+# BloomZ: No chat template is set for this tokenizer, falling back to a default class-level template. This is very error-prone, because models are often trained with templates different from the class default! Default chat templates are a legacy feature and will be removed in Transformers v4.43, at which point any code depending on them will stop working. We recommend setting a valid chat template before then to ensure that this model continues working without issues.
+            
