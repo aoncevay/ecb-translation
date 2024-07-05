940182aec4a68fc48ebeb11fa309c4eaf0022fc6
Arturo Oncevay <a.oncevay@ed.ac.uk>
2024-07-05 14:51:55 +0100

    save sample

--- read.py
+++ read.py
@@ -16,3 +16,18 @@ def load_dataset(
         print(lang, len(dataset[lang]))
 
     return dataset
+
+
+def save_sample(
+        filename_prefix : str ="./data_multi/multi.ECB",
+        outdir_prefix : str = "./data_smpl50/multi.ECB",
+        sample: int = 50) -> None:
+    
+    for lang, _ in languages + [("en", "eng_Latn")]:
+        filename = f'{filename_prefix}.{lang}.txt'
+        filename_out = f"{outdir_prefix}.{lang}.txt"
+        with open(filename, "r", encoding="utf-8") as f, open(filename_out, "w", encoding="utf-8") as f_out:
+            lines = f.readlines()
+            sample_lines = [l.strip() for l in lines][-sample:]
+            f_out.write("\n".join(sample_lines))
+    return
