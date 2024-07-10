eb05e00c26109e809e67a801497929874c6ec3e5
Arturo Oncevay <a.oncevay@ed.ac.uk>
2024-07-10 13:58:36 +0100

    comet model

--- comet.sh
+++ comet.sh
@@ -17,8 +17,8 @@ for lang in $languages; do
     src_file="${prefix_ref}/ECB.en.txt"
     ref_file="${prefix_ref}/ECB.$lang.txt"
     hyp_file="${prefix_out}/$system.en2$lang.txt"
-    comet-score -s ${src_file} -t ${hyp_file} -r ${ref_file} --model Unbabel/wmt22-cometkiwi-da --gpus 1 --only_system
+    comet-score -s ${src_file} -t ${hyp_file} -r ${ref_file} --model Unbabel/wmt22-comet-da --gpus 1 --only_system
     #xx2en
     hyp_file="${prefix_out}/$system.${lang}2en.txt"
-    comet-score -s ${ref_file} -t ${hyp_file} -r ${src_file} --model Unbabel/wmt22-cometkiwi-da --gpus 1 --only_system
+    comet-score -s ${ref_file} -t ${hyp_file} -r ${src_file} --model Unbabel/wmt22-comet-da --gpus 1 --only_system
 done
